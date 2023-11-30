55c1c80203a111b62496976fdfbb5c297860e675
Leah Yoimiya <leahjb06.kodehode@gmail.com>
2023-11-30 14:33:29 +0100

    did first draft of shop ready for api

---
+++ src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,16 @@
+import Styles from "../../Pages/Shop/Shop.module.css";
+
+export function ProductCard({ product }) {
+  return (
+    <div className={Styles.cardProduct}>
+      <img src={product.image} alt={product.title} className={Styles.productImg} />
+      <div className={Styles.containProduct}>
+        <p>{product.title}</p>
+        <p>{product.price}$</p>
+        <p className={Styles.descStyles}>{product.description}</p>
+        <p>Quantity: + 0 -</p>
+        <button className={Styles.productBtn}>Add to cart</button>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,7 +1,28 @@
+import Styles from "./Shop.module.css";
+import { useEffect, useState } from "react";
+import { ProductCard } from "../../Components/ProductCard/ProductCard";
+
 export default function Shop() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = "https://fakestoreapi.com/products";
+
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
-    <>
-      <div></div>
-    </>
+    <div className={Styles.productGrid}>
+      {products.map((product, index) => (
+        <ProductCard key={index} product={product} />
+      ))}
+    </div>
   );
 }

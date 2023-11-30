3b37b5b2966109c06186e53ccc4682fc86b23f24
Stein-B06 <Steinb06.kodehode@gmail.com>
2023-11-30 10:36:17 +0100

    updated classnames

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,18 @@
-import "./Header.css";
 import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 export function Header() {
   return (
     <>
-      <div className="header--container">
+      <div className={styles.headerContainer}>
         <h1>DD</h1>
         <p>DesDerio</p>
         <img src="" alt="User Icon" />
         <img src="" alt="Shopping cart" />
         <img src="" alt="Magnifying glass" />
         <img src="" alt="Hamburger" />
-        <ul className="pagesList" id="pagesList">
+
+        <ul className={styles.pagesList}>
           <li>
             <Link to="/" className="pages">
               Home

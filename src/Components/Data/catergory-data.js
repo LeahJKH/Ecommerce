fbe41faab0e472e9f9a09c34a956600ab0068f9b
JesperKoder <jesper.kodehode@gmail.com>
2023-12-01 14:22:31 +0100

    category page is finished?

---
+++ src/Components/Data/catergory-data.js
@@ -0,0 +1,8 @@
+export const categories = [
+  { name: "Boats", image: "../../../public/pictures/boats.png" },
+  { name: "Cars", image: "../../../public/pictures/cars.png" },
+  { name: "Clothes", image: "../../../public/pictures/clothes.png" },
+  { name: "Houses", image: "../../../public/pictures/houses.png" },
+  { name: "Companies", image: "../../../public/pictures/companies.png" },
+  { name: "Jewelry", image: "../../../public/pictures/jewelry.png" },
+];

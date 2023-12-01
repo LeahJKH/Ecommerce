2acb165835d79836a762f39b9f864484022ed727
Leah Yoimiya <leahjb06.kodehode@gmail.com>
2023-12-01 12:11:09 +0100

    Started the burger

---
+++ src/Components/HamburgerMenu/HamburgerUserClick.jsx
@@ -0,0 +1,17 @@
+export default function Hamburger() {
+  return (
+    <>
+      <div>
+        <h1>X</h1>
+        <img src="../../../public/icons/person.svg" alt="User icon" />
+        <h6>Name</h6>
+        <p>View profile</p>
+        <img src="../../../public/icons/Settings.svg" alt="" />
+        <h6>Settings</h6>
+        <img src="../../../public/icons/Loggout.svg" alt="" />
+        <p>Log out</p>
+        <p></p>
+      </div>
+    </>
+  );
+}

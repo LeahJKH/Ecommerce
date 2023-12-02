e83a03570a98e1b12a688c2907cfb1e81c729da1
FerhatO-H04 <131246514+FerhatO-H04@users.noreply.github.com>
2023-12-02 15:23:08 +0100

    created loggInnPage

--- src/Components/Logginn/LoggInn.jsx
+++ src/Components/Logginn/LoggInn.jsx
@@ -1,7 +1,46 @@
-export default function LoggInn() {
+import styles from'./LoggInn.module.css'
+
+export function LoggInn() {
   return (
+    
     <>
-      <div></div>
+      <div className={styles.loggInn}>
+      <div className={styles.loggInnContainer}>
+        <h3 className={styles.singUpTitle}>Sing Up</h3>
+        <form action="" className={styles.singUpForm}>
+
+        
+            <svg className={styles.icon} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="mdi:user">
+              <path id="Vector" d="M12 4C13.0609 4 14.0783 4.42143 14.8284 5.17157C15.5786 5.92172 16 6.93913 16 8C16 9.06087 15.5786 10.0783 14.8284 10.8284C14.0783 11.5786 13.0609 12 12 12C10.9391 12 9.92172 11.5786 9.17157 10.8284C8.42143 10.0783 8 9.06087 8 8C8 6.93913 8.42143 5.92172 9.17157 5.17157C9.92172 4.42143 10.9391 4 12 4ZM12 14C16.42 14 20 15.79 20 18V20H4V18C4 15.79 7.58 14 12 14Z" fill="black"/>
+            </g>
+            </svg>
+            <input type="text" name='name' className={styles.formName} id='formName' placeholder='Name' />
+          
+
+         
+            <svg className={styles.icon} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="ic:baseline-email">
+              <path id="Vector" d="M20 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM20 8L12 13L4 8V6L12 11L20 6V8Z" fill="black"/>
+            </g>
+            </svg>
+            <input type="email" name='email' className={styles.formEmail} id='formEmail' placeholder='Email' />
+        
+
+          
+            <svg className={styles.icon} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="mdi:password">
+            <path id="Vector" d="M12 17C12.5304 17 13.0391 16.7893 13.4142 16.4142C13.7893 16.0391 14 15.5304 14 15C14 14.4696 13.7893 13.9609 13.4142 13.5858C13.0391 13.2107 12.5304 13 12 13C11.4696 13 10.9609 13.2107 10.5858 13.5858C10.2107 13.9609 10 14.4696 10 15C10 15.5304 10.2107 16.0391 10.5858 16.4142C10.9609 16.7893 11.4696 17 12 17ZM18 8C18.5304 8 19.0391 8.21071 19.4142 8.58579C19.7893 8.96086 20 9.46957 20 10V20C20 20.5304 19.7893 21.0391 19.4142 21.4142C19.0391 21.7893 18.5304 22 18 22H6C5.46957 22 4.96086 21.7893 4.58579 21.4142C4.21071 21.0391 4 20.5304 4 20V10C4 9.46957 4.21071 8.96086 4.58579 8.58579C4.96086 8.21071 5.46957 8 6 8H7V6C7 4.67392 7.52678 3.40215 8.46447 2.46447C9.40215 1.52678 10.6739 1 12 1C12.6566 1 13.3068 1.12933 13.9134 1.3806C14.52 1.63188 15.0712 2.00017 15.5355 2.46447C15.9998 2.92876 16.3681 3.47995 16.6194 4.08658C16.8707 4.69321 17 5.34339 17 6V8H18ZM12 3C11.2044 3 10.4413 3.31607 9.87868 3.87868C9.31607 4.44129 9 5.20435 9 6V8H15V6C15 5.20435 14.6839 4.44129 14.1213 3.87868C13.5587 3.31607 12.7956 3 12 3Z" fill="black"/>
+            </g>
+            </svg>
+            <input type="password" name='password' className={styles.formPassword} id='formPassword' placeholder='Password'/>
+         
+
+          <button type='submit' className={styles.formSingUpButton} id='formSingUpButton'>Sing Up</button>
+        </form>
+        <p className={styles.singInText}>Already have an account? <a href="#">Sign in</a></p>
+      </div>
+      </div>
     </>
   );
 }

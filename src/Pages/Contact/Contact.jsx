48cf9a82d339b387ab4d7588fd3b21d3e4aa719e
Stein-B06 <Steinb06.kodehode@gmail.com>
2023-12-04 09:16:22 +0100

    Created contactpage, and added CSS

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,55 @@
-import "./Contact.css";
+import style from "./Contact.module.css";
 export function Contact() {
   return (
     <>
-      <div className="contactPage--container">
-        <h1>Contact Page-- Main</h1>
+      <div className={style.contactPagecontainer}>
+        <div className={style.headertext}>
+          <h1>Contact the creators of Desiderio</h1>
+          <p>
+            We have the <span>worlds best customer service. </span>Subscribe to
+            our premium membership, and we will respond within 5 minutes. Feel
+            free to contact us by filling out the contact form below:
+          </p>
+        </div>
+        <div className={style.contactformcontainer}>
+          <form className={style.contactform}>
+            <label className={style.formlabel} htmlFor="name">
+              Name:
+            </label>
+            <input
+              className={style.forminput}
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your Name"
+              required
+            />
+
+            <label htmlFor="email">Email:</label>
+            <input
+              className={style.forminput}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your Email"
+              required
+            />
+
+            <label htmlFor="message">Your message:</label>
+            <textarea
+              className={style.formtextarea}
+              id="message"
+              name="message"
+              placeholder="Your Message"
+              rows="6"
+              required
+            ></textarea>
+
+            <button className={style.sendbutton} type="submit">
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );

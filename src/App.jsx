05fe50616b8c38e152695b86296e1a2061e6f643
Leah Yoimiya <leahjb06.kodehode@gmail.com>
2023-12-04 13:43:42 +0100

    made catagory and shop one thing

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Userpage from "./Pages/UserPage/UserPage";
 import { Category } from "./Pages/Catagory/Catagory";
 import { Contact } from "./Pages/Contact/Contact";
 import Settings from "./Pages/Settings/Settings";
-import Shop from "./Pages/Shop/Shop";
+
 import ShoppingCart from "./Pages/ShoppingCart/ShoppingCart";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -18,7 +18,7 @@ function App() {
           <Route path="/UserPage" element={<Userpage />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/Catagories" element={<Category />} />
-          <Route path="/Shop" element={<Shop />} />
+
           <Route path="/ShoppingCart" element={<ShoppingCart />} />
           <Route path="/Settings" element={<Settings />} />
           <Route path="*" element={<h1>404 Not Found</h1>} />
--- src/Pages/Catagory/Catagory.jsx
+++ src/Pages/Catagory/Catagory.jsx
@@ -1,28 +1,89 @@
-import Style from "./Catagory.module.css";
+import { useState, useEffect } from "react";
+import Styles from "./catagory.module.css";
+import Style from "./Shop.module.css";
+import { ProductCard } from "../../Components/ProductCard/ProductCard";
 
 const categories = [
   { name: "Boats", image: "../../../public/pictures/boats.png" },
   { name: "Cars", image: "../../../public/pictures/cars.png" },
-  { name: "Clothes", image: "../../../public/pictures/clothes.png" },
+  { name: "men's clothing", image: "../../../public/pictures/clothes.png" },
   { name: "Houses", image: "../../../public/pictures/houses.png" },
   { name: "Companies", image: "../../../public/pictures/companies.png" },
   { name: "Jewelry", image: "../../../public/pictures/jewelry.png" },
 ];
 
 export function Category() {
-  const [Changed, noChange] = useState("");
-  function HandleCatagoryChange() {}
-  const apiUrl = "https://fakestoreapi.com/products";
-  (categories.name || apiUrl.category).toLowerCase().includes(Changed);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = "https://fakestoreapi.com/products";
+
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  function handleCategoryChange(categoryName) {
+    setSelectedCategory(categoryName);
+  }
+
   return (
-    <div className={Style.category}>
+    <div className={Styles.category}>
       <h2>Category Page</h2>
-      <div className={Style.categoryContainer}>
+      <div className={Styles.categoryContainer}>
         {categories.map((category, index) => (
-          <div key={index} className={Style.categoryCard}>
-            <h3 onClick={HandleCatagoryChange}>{category.name}</h3>
-            <img className={Style.cardImage} src={category.image} alt={category.name} />
+          <div key={index} className={Styles.categoryCard}>
+            <h3 onClick={() => handleCategoryChange(category.name)}>{category.name}</h3>
+            <img className={Styles.cardImage} src={category.image} alt={category.name} />
           </div>
+        ))}
+      </div>
+      <div className={Style.centering}>
+        <div className={Style.cardFlex}>
+          {products
+            .filter(
+              (product) =>
+                !selectedCategory ||
+                product.category.toLowerCase() === selectedCategory.toLowerCase()
+            )
+            .map((product, index) => (
+              <ProductCard key={index} product={product} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// The Shop component remains unchanged and is exported as a named export
+export function Shop({ selectedCategory }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = "https://fakestoreapi.com/products";
+
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        const filteredProducts = selectedCategory
+          ? data.filter(
+              (product) => product.category.toLowerCase() === selectedCategory.toLowerCase()
+            )
+          : data;
+        setProducts(filteredProducts);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [selectedCategory]);
+
+  return (
+    <div className={Style.centering}>
+      <div className={Style.cardFlex}>
+        {products.map((product, index) => (
+          <ProductCard key={index} product={product} />
         ))}
       </div>
     </div>

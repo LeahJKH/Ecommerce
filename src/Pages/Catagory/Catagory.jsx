4255ec9617316efa0d2f3747f0dc9a398db4b97c
Leah Yoimiya <leahjb06.kodehode@gmail.com>
2023-12-04 11:01:48 +0100

    trying too fix catagories

--- src/Pages/Catagory/Catagory.jsx
+++ src/Pages/Catagory/Catagory.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Style from "./Catagory.module.css";
+import Shop from "../Shop/Shop";
 
 const categories = [
   { name: "Boats", image: "../../../public/pictures/boats.png" },
@@ -11,18 +13,25 @@ const categories = [
 ];
 
 export function Category() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category.name);
+  };
+
   return (
     <div className={Style.category}>
       <h2>Category Page</h2>
       <div className={Style.categoryContainer}>
         {categories.map((category, index) => (
-          <div key={index} className={Style.categoryCard}>
+          <div className={Style.categoryCard} key={index}>
             <h3>{category.name}</h3>
-            <img
-              className={Style.cardImage}
-              src={category.image}
-              alt={category.name}
-            />
+            <img className={Style.cardImage} src={category.image} alt={category.name} />
+
+            {/* Pass selectedCategory to Shop component */}
+            <Link to={`/shop/${category.name}`} onClick={() => handleCategoryClick(category)}>
+              View Products
+            </Link>
           </div>
         ))}
       </div>
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,9 +1,9 @@
-import Styles from "./Shop.module.css";
 import { useEffect, useState } from "react";
-import { ProductCard } from "../../Components/ProductCard/ProductCard";
+import { useParams } from "react-router-dom";
 
 export default function Shop() {
   const [products, setProducts] = useState([]);
+  const { categoryName } = useParams();
 
   useEffect(() => {
     const apiUrl = "https://fakestoreapi.com/products";
@@ -11,18 +11,25 @@ export default function Shop() {
     fetch(apiUrl)
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data);
+        const filteredProducts = categoryName
+          ? data.filter((product) => product.category.toLowerCase() === categoryName.toLowerCase())
+          : data;
+        setProducts(filteredProducts);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, []);
+  }, [categoryName]);
 
   return (
-    <div className={Styles.centering}>
-      <div className={Styles.cardFlex}>
-        {products.map((product, index) => (
-          <ProductCard key={index} product={product} />
+    <div>
+      <h2>{categoryName ? categoryName : "All Products"}</h2>
+      <div>
+        {products.map((product) => (
+          <div key={product.id}>
+            <h3>{product.title}</h3>
+            {/* Display other product information */}
+          </div>
         ))}
       </div>
     </div>

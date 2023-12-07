27131d7ec7f831d503bb03ac256e02cc5e84055c
LeahJKH <leahjb06.kodehode@gmail.com>
2023-12-07 12:56:14 +0100

    pls

--- src/Pages/Catagory/Catagory.jsx
+++ src/Pages/Catagory/Catagory.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Styles from "/catagory.module.css";
+import Styles from "./catagory.module.css";
 
 import { ProductCard } from "../../Components/ProductCard/ProductCard";
 

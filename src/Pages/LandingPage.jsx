15f0053f605e920bd954672453e8b138ed745f66
JesperKoder <jesper.kodehode@gmail.com>
2023-12-07 12:04:38 +0100

    trying to fix carousel

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,64 +2,37 @@ import "./LandingPage.css"
 import boat from "../../public/pictures/boats.png"
 
 
+
 export function LandingPage() {
-  const handleLinkClick = () => {
-    return false;
-  }
 
   return (
     <>
       <div className="mainContent--container">
         <h1 className="main--title">Popular Items</h1>
-        <div className="carousel-container">
-          <div className="carousel">
-            <div id="skyline" className="carousel__item">
+        <div className="carousel">
+          <button className="carousel-button prev">
+            &#8656;
+          </button>
+          <button className="carousel-button next">
+            &#8658;
+          </button>
+          <ul data-slides>
+            <li className="slide">
+              <img src={boat} alt="boat" />
+            </li>
+            <li className="slide">
               <img
-                src="https://images.unsplash.com/photo-1474181487882-5abf3f0ba6c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=960&h=420&q=80"
-                alt="Skyline of Wai Tan, Shanghai"
-                className="carousel__image"
+                src="https://images.unsplash.com/photo-1658409524023-5c835864baf2?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="boat"
               />
-            </div>
-            <div id="great-wall-of-china" className="carousel__item">
+            </li>
+            <li className="slide">
               <img
-                src="https://images.unsplash.com/photo-1508804185872-d7badad00f7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=960&h=420&q=80"
-                alt="Great wall of China."
-                className="carousel__image"
+                src="https://images.unsplash.com/photo-1633596683562-4a47eb4983c5?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="boat"
               />
-            </div>
-            <div id="sunset-on-the-li-river" className="carousel__item">
-              <img
-                src="https://images.unsplash.com/photo-1543097692-fa13c6cd8595?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=960&h=420&q=80"
-                alt="Sunset on the Li River as the few remaining cormorant fisherman pack their."
-                className="carousel__image"
-              />
-            </div>
-          </div>
-          <div id="controls" className="controls">
-            <a
-              onClick={handleLinkClick}
-              href="#skyline"
-              className="controls__dot"
-            >
-              <span className="visuallyhidden">
-                Skyline of Wai Tan, Shanghai
-              </span>
-            </a>
-            <a
-              onClick={handleLinkClick}
-              href="#great-wall-of-china"
-              className="controls__dot"
-            >
-              <span className="visuallyhidden">Great wall of China</span>
-            </a>
-            <a
-              onClick={handleLinkClick}
-              href="#sunset-on-the-li-river"
-              className="controls__dot"
-            >
-              <span className="visuallyhidden">Sunset on the Li River</span>
-            </a>
-          </div>
+            </li>
+          </ul>
         </div>
 
         <h1>Categories</h1>
